ffe1ff89396ebd918ff6719bcc8bee73185c9ff3
0tak2.code <0tak2.code@gmail.com>
2023-01-24 09:32:31 +0900

    BOJ1406 연결리스트 직접 구현 풀이

--- BOJ/boj1406.cc
+++ BOJ/boj1406.cc
@@ -3,31 +3,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MX = 10001;
+const int MX = 600001;
 char dat[MX] = {0};
-int pre[MX] = {-1};
-int nxt[MX] = {-1};
+int pre[MX];
+int nxt[MX];
 int unused = 1;
+int cursor = 0;
 
-void push_left(int idx, char val) {
+void push(int idx, char val) {
+    dat[unused] = val;
+    pre[unused] = idx;
+    nxt[unused] = nxt[idx];
+
+    if (nxt[idx] != -1)
+        pre[nxt[idx]] = unused;
+    nxt[idx] = unused;
+
+    cursor = unused;
+    unused++;
 }
 
 void erase(int idx) {
+    if (idx == 0)
+        return;
+
+    nxt[pre[idx]] = nxt[idx];
+    if (nxt[idx] != -1)
+        pre[nxt[idx]] = pre[idx];
+    cursor = pre[idx];
+}
+
+void show() {
+    int cursor = nxt[0];
+    while(cursor != -1) {
+        cout << dat[cursor];
+        cursor = nxt[cursor];
+    }
 }
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
+
+    fill(pre, pre+MX, -1);
+    fill(nxt, nxt+MX, -1);
+
     string origin;
     int N;
 
     cin >> origin >> N;
     
+    nxt[0] = 1;
     for (int i=0; i<origin.size(); i++) {
         dat[i+1] = origin.at(i);
         pre[i+1] = i;
-        if (i != origin.size()-1)
+        if (i != origin.size()-1) {            
             nxt[i+1] = i+2;
+        } 
+        cursor = i + 1;
         unused++;
     }
 
@@ -36,9 +69,22 @@ int main(void) {
         char target;
 
         cin >> cmd;
-        if (cmd == 'P')
+        if (cmd == 'P') {
             cin >> target;
+            push(cursor, target);
+        } else if (cmd == 'L') {
+            if (pre[cursor] != -1) 
+                cursor = pre[cursor];
+        } else if (cmd == 'D') {
+            if (nxt[cursor] != -1)
+                cursor = nxt[cursor];
+        } else if (cmd == 'B') {
+            erase(cursor);
+        }
 
     }
 
+    show();
+
+    return 0;
 }
