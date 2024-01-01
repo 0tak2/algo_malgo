533ee8450aec4276bd5f2dd93c938dced94e332d
0tak2 <0tak2.code@gmail.com>
2024-01-01 15:35:45 +0900

    icote: 5장 문제 풀이

--- icote-java/src/main/java/kr/pe/otag2/study/icote/ch5/Maze_5_4.java
+++ icote-java/src/main/java/kr/pe/otag2/study/icote/ch5/Maze_5_4.java
@@ -3,9 +3,7 @@ package kr.pe.otag2.study.icote.ch5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 /**
