e8c398c53e060c2c669e23cb747b8533cfc9babc
0tak2 <0tak2.code@gmail.com>
2024-01-24 21:57:21 +0900

    이코테 6장 정렬 예제

---
+++ icote-java/src/main/java/kr/pe/otag2/study/icote/ch6/TwoArrays_6_4.java
@@ -0,0 +1,46 @@
+package kr.pe.otag2.study.icote.ch6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class TwoArrays_6_4 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = inputs[0];
+        int k = inputs[1];
+
+        Integer[] array1 = new Integer[n];
+        Integer[] array2 = new Integer[n]; // Collections.reverseOrder() 사용 위해 Integer 배열로 정의
+
+        String[] arrayRaw1 = br.readLine().split(" ");
+        for (int i=0; i<n; i++) {
+            array1[i] = Integer.parseInt(arrayRaw1[i]);
+        }
+
+        String[] arrayRaw2 = br.readLine().split(" ");
+        for (int i=0; i<n; i++) {
+            array2[i] = Integer.parseInt(arrayRaw2[i]);
+        }
+
+        Arrays.sort(array1);
+        Arrays.sort(array2, Collections.reverseOrder());
+
+        for (int i=0; i<k; i++) {
+            int tmp = array1[i];
+            array1[i] = array2[i];
+            array2[i] = tmp;
+        }
+
+        System.out.println(Arrays.stream(array1).reduce(0, Integer::sum));
+        // System.out.println(Arrays.stream(array1).mapToInt(i -> i).sum()); // IntStream으로 변환하여 sum
+    }
+
+    public static void main(String[] args) throws IOException {
+        TwoArrays_6_4 solution = new TwoArrays_6_4();
+        solution.solution();
+    }
+}
