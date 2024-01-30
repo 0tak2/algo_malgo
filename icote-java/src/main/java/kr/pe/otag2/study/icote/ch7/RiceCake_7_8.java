dab5151c79212a33f2ab67cd5ffac2c0df4bf19b
0tak2 <0tak2.code@gmail.com>
2024-01-30 22:16:58 +0900

    7장 풀이

--- icote-java/src/main/java/kr/pe/otag2/study/icote/ch7/RiceCake_7_8.java
+++ icote-java/src/main/java/kr/pe/otag2/study/icote/ch7/RiceCake_7_8.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class RiceCake_7_8 {
     public void solution() throws IOException {
@@ -15,17 +14,22 @@ public class RiceCake_7_8 {
         int[] riceCakes = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         int start = 0;
-        while (height > 0) {
-            int heightSnapshot = height;
-            int sum = Arrays.stream(riceCakes).map(i -> i - heightSnapshot).filter(i -> i > 0).sum();
-            if (sum >= sumTarget) {
-                System.out.println(height);
-                return;
-            }
+        int end = Arrays.stream(riceCakes).max().orElse(-1);
+        int heightCandidate = -1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            final int finalMid = mid;
 
-            height = height / 2;
+            int sum = Arrays.stream(riceCakes).map(i -> i - finalMid).filter(i -> i > 0).sum();
+            if (sum < sumTarget) {
+                end = mid - 1; // 덜 잘라야 함 (height 줄이기)
+            } else {
+                heightCandidate = mid; // 정답 가능성 있음. 정답인 경우 다음에 루프에서 빠져나가게 됨
+                start = mid + 1; // 더 많이 잘라야 함 (height 늘리기)
+            }
         }
 
+        System.out.println(heightCandidate);
     }
 
     public static void main(String[] args) throws IOException {
