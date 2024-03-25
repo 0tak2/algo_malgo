66cbee66fe546619e329af5b13d72fa70b2550e2
0tak2 <0tak2.code@gmail.com>
2024-03-25 21:23:28 +0900

    prog: 60059 wip

--- prog-java/src/main/java/kr/pe/otag2/study/impl/LockAndKey_60059.java
+++ prog-java/src/main/java/kr/pe/otag2/study/impl/LockAndKey_60059.java
@@ -1,5 +1,10 @@
 package kr.pe.otag2.study.impl;
 
+/**
+ * fixme
+ * 정확성 테스트 73.0 / 100.0
+ * 실패: 1, 23 ~ 33
+ */
 public class LockAndKey_60059 {
     public int[][] rotate90(int[][] original) {
         int d = original.length;
@@ -15,8 +20,41 @@ public class LockAndKey_60059 {
     }
 
     public boolean solution(int[][] key, int[][] lock) {
-        boolean answer = true;
-        return answer;
+        if (key.length > lock.length) {
+            return false;
+        }
+
+        int expandedDimension = lock.length + (key.length - 1) * 2; // 한 줄은 겹치게 된다.
+        int[][] expandedLock = new int[expandedDimension][expandedDimension];
+        for (int h = 0; h < lock.length; h++) {
+            for (int w = 0; w < lock.length; w++) {
+                int value = lock[h][w];
+                expandedLock[key.length - 1 + h][key.length - 1 + w] = value; // 그려보기
+            }
+        }
+
+        boolean found = false;
+        // 회전
+        for (int i = 0; i < 4; i++) {
+            int[][] rotatedKey = rotate90(key);
+            // 시작점
+            for (int h = 0; h <= expandedLock.length - key.length; h++) {
+                for (int w = 0; w <= expandedLock.length - key.length; w++) {
+                    SEARCH:
+                    for (int j = 0; j < key.length; j++) {
+                        for (int k = 0; k < key.length; k++) {
+                            if (rotatedKey[j][k] == expandedLock[h + j][w + k]) {
+                                continue SEARCH;
+                            }
+                        }
+                    }
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        return found;
     }
 
     public void toString2dArr(int[][] arr) {
