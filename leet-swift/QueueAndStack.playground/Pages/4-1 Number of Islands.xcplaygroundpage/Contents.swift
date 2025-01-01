b4bbb2d778e78c5fb84e6c2d66a7b40f9eb324a9
0tak2 <0tak2.code@gmail.com>
2025-01-01 20:25:33 +0900

    leet-swift: Queue & Stack - DFS wip

---
+++ leet-swift/QueueAndStack.playground/Pages/4-1 Number of Islands.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Number of Islands
+// https://leetcode.com/explore/learn/card/queue-stack/232/practical-application-stack/1380/
+/**
+ 
+ */
+
+class Solution {
+    func numIslands(_ grid: [[Character]]) -> Int {
+        
+    }
+}
+
+//: [Next](@next)
