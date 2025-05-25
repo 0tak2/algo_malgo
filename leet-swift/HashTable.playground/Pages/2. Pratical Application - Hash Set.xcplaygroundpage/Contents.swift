4c52c9de7325de93342eab7361c63063dc08d3f7
0tak2 <0tak2.code@gmail.com>
2025-05-25 11:40:08 +0900

    [leet-swift] Happy Number

--- leet-swift/HashTable.playground/Pages/2. Pratical Application - Hash Set.xcplaygroundpage/Contents.swift
+++ leet-swift/HashTable.playground/Pages/2. Pratical Application - Hash Set.xcplaygroundpage/Contents.swift
@@ -94,4 +94,63 @@ func intersection2(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
     return Array(res)
 }
 
+// MARK: - Happy Number
+// https://leetcode.com/explore/learn/card/hash-table/183/combination-with-other-algorithms/1131/
+// 해피넘버인지 판별하는 문제이다.
+// 해피넘버는 자릿수 각 수를 제곱해 더하는 일을 반복했을 때 1이 되는 수를 말한다.
+// 1이 안되는 경우 다시 자기 자신이 되어 무한 루프가 되는데, 이런 수는 언해피넘버라고 한다. 신기하다.
+
+// Trial 1
+// 처음에는 무한루프라는 것에서 플로이드 알고리즘으로 풀었다.
+// 3ms, 20.1MB
+func separateDigits(_ n: Int) -> [Int] {
+    return String(n).compactMap { $0.wholeNumberValue }
+}
+
+func nextNumber(_ n: Int) -> Int {
+    let digits = separateDigits(n)
+    
+    var result = 0 // reduce로 하면 될텐데 자동완성 없이 인자 순서와 레이블을 모르겠다...
+    for d in digits {
+        result += d * d
+    }
+    
+    return result
+}
+
+func isHappy(_ n: Int) -> Bool {
+    var slow = n
+    var fast = nextNumber(n)
+    
+    while slow != fast {
+        slow = nextNumber(slow)
+        fast = nextNumber(nextNumber(fast))
+        
+        print("slow: \(slow)")
+        print("fast: \(fast)")
+    }
+    
+    print()
+    return slow == 1
+}
+
+// Trial 2
+// 이게 왜 해시셋 문제로 나오지? 의아했는데 제출된 샘플 코드를 보니
+// 해시셋에 계산했던 수를 담아서 다시 나오는지를 보는 식으로 사용했다.
+// 플로이드 알고리즘은 두 개의 포인터를 쓰면서 이중으로 계산하기 떄문에
+// 이 방식이 공간복잡도는 높아도 평균적인 처리 속도는 빨라지는 것 같다.
+// 1ms, 20.2MB
+func isHappy2(_ n: Int) -> Bool {
+    var cur = n
+    
+    var seen = Set<Int>()
+    
+    while !seen.contains(cur) {
+        seen.insert(cur)
+        cur = nextNumber(cur)
+    }
+    
+    return cur == 1
+}
+
 //: [Next](@next)
